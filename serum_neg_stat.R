ff57a0ebff0c10833d43328a0d8ffe87817ff59b
wizard <wizard@pop-os.localdomain>
2022-03-18 21:48:52 +0800

    new

--- serum_neg_stat.R
+++ serum_neg_stat.R
@@ -172,6 +172,15 @@ name_df <- gather_inchi_curl() %>%
 indo_export <- simp_candi %>% 
   merge(name_df, by = "sp.id", all.x = T) %>% 
   dplyr::mutate(name = ifelse(name == "null", IUPACName, name)) %>%
+  dplyr::select(.id, name, classification, tanimotoSimilarity) %>% 
+  dplyr::rename(id = .id, info = classification) %>% 
+  merge(export, by = "id", all.y = T) %>% 
+  dplyr::filter(is.na(name) == F) %>% 
+  dplyr::arrange(desc(tanimotoSimilarity)) %>% 
   dplyr::as_tibble()
+## ------------------------------------- 
+## mannualy adjust name >>> syno_backup
+gt_indo_export <- indo_export %>% 
+  pretty_table(spanner = T, shorter_name = F, default = T)
 ## ---------------------------------------------------------------------- 
 ## ---------------------------------------------------------------------- 
