463d4b4ffe593aa87d933988d689aacfca831281
wizard <wizard@pop-os.localdomain>
2022-04-11 09:14:48 +0800

    new

--- add_iso_stat_accuracy/simulate_to_gnps.R
+++ add_iso_stat_accuracy/simulate_to_gnps.R
@@ -4,12 +4,13 @@
 msp <- "/media/wizard/back/test_mcnebula/gnps_pos/mcnebula_results/gnps/msms_pos_gnps.msp"
 ## set function to get gnps mgf
 msp_to_mgf(get_filename(msp),
-           id_prefix = "gnps",
+           id_prefix = "",
            path = get_path(msp),
            fun = "deal_with_msp_record",
            pre_modify = F,
            mass_level = 2,
-           set_rt = 1000)
+           set_rt = 1000,
+           add_scans = T)
 ## get_metadata
 gnps_metadata <- read_tsv(paste0(msp, ".meta.tsv"))
 ## simulate quant csv
