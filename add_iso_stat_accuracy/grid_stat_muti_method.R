34740f746e811e235fa7a3974a8e3234b9c0b430
wizard <wizard@pop-os.localdomain>
2022-04-12 08:37:36 +0800

    new

--- add_iso_stat_accuracy/grid_stat_muti_method.R
+++ add_iso_stat_accuracy/grid_stat_muti_method.R
@@ -12,8 +12,20 @@ envir_name <- c("origin", "noise", "h_noise")
 lapply(envir_name, function(name){
          assign(name, new.env(), envir = parent.frame(2))
 })
+## ------------------------------------- 
+## extract var
+var <- c("dominant_stat", "extra_dominant",
+         "molnet_dominant_stat", "m_extra_dominant")
 ## ---------------------------------------------------------------------- 
 path <- c("/media/echo/DATA/yellow/iso_gnps_pos",
           "/media/echo/DATA/yellow/noise_gnps_pos",
-          "/media/echo/h_noise_gnps_pos")
+          "/media/echo/DATA/yellow/h_noise_gnps_pos")
+## ---------------------------------------------------------------------- 
+## batch extraction
+pbapply::pbmapply(function(path, envir_name, var){
+                    load(paste0(path, "/", ".RData"))
+                    lapply(var, function(var){
+                             assign(var, eval(parse(text = var)), envir = get(envir_name)) }
+                    )}, path, envir_name,
+          MoreArgs = list(var = var))
 ## ---------------------------------------------------------------------- 
