479505a958a6bef597ffd0b443e5cb080c3f7de7
echo <echo@pop-os.localdomain>
2022-04-14 14:53:10 +0800

    revise

--- mcnebula_cell_validation/get_metadata.R
+++ mcnebula_cell_validation/get_metadata.R
@@ -17,12 +17,14 @@ metadata <- dataset %>%
   data.table::rbindlist(idcol = T) %>% 
   dplyr::rename(group = .id) %>% 
   ## other attributes supplementation
-  dplyr::mutate(path = paste0(group, "/", sample),
+  dplyr::mutate(`Raw data` = sample,
+                path = paste0(group, "/", sample),
                 first_seq = stringr::str_extract(sample, "(?<=Plate_)[0-9]{1,}(?=_)"),
                 identifier = stringr::str_extract(sample, "(?<=_)[A-Z]{2}[0-9]{1,}(?=_)"),
                 ## subgroup
                 subgroup = stringr::str_extract(identifier, "[A-Z]{1,}"),
-                sufix_seq = stringr::str_extract(sample, "(?<=_)[0-9]{1,}(?=\\.)"))
+                sufix_seq = stringr::str_extract(sample, "(?<=_)[0-9]{1,}(?=\\.)")) %>%
+  dplyr::relocate(`Raw data`)
 ## write metadata
 write.table(metadata, file = "metadata.csv", sep = ",", row.names = F, col.names = T, quote = F)
 
