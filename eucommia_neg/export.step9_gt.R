b095c9f2ffa4841deeb15f14bb722f059b47101c
wizard <wizard@pop-os.localdomain>
2022-04-23 08:53:28 +0800

    new

--- eucommia_neg/export.step9_gt.R
+++ eucommia_neg/export.step9_gt.R
@@ -14,5 +14,15 @@ gt_table <- pretty_table(dplyr::rename(export.dominant[, -ncol(export.dominant)]
                locations = cells_column_labels(columns = `Tanimoto similarity`)) %>% 
   ## InChIKey
   tab_footnote(footnote = "The 'InChIKey planar' is the first hash block of InChIKey that represents a molecular skeleton.",
-               locations = cells_column_labels(columns = `InChIKey planar`))
+               locations = cells_column_labels(columns = `InChIKey planar`)) %>% 
+  tab_footnote(footnote = "All identified formulae are in adduct of '[M - H]-'.",
+               locations = cells_column_labels(columns = `Formula`)) %>% 
+  tab_footnote(footnote = "Id are generated while MZmine2 processing and are inherited in subsequent MCnebula workflow.",
+               locations = cells_column_labels(columns = `Id`)) %>% 
+  tab_footnote(footnote = "The mass error are calculated by SIRIUS while predicting formula of compounds.",
+               locations = cells_column_labels(columns = `Mass error (ppm)`)) %>% 
+  tab_footnote(footnote = "The variation are difined as: ↑ or ↓ indicates increasing or decreasing of compound content after E. ulmoides being processed, whereas - indicates no apparent alteration; and the number of arrow imply the magnitude of alteration, i.e., integer of |log2(FC)|. FC is defined as Pro-Eucommia compare with Raw-Eucommia.",
+               locations = cells_column_labels(columns = `Variation`))
+
+
 
